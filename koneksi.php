24d0662faed24ae4eaf9331e3085dae5216bf6ec
redoapriyoga3-coder <redoapriyoga3@gmail.com>
2026-07-10 13:00:29 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,14 @@
 <?php
-$url = getenv('DATABASE_URL');
-if (!$url) { die("Error: DATABASE_URL tidak diset di Railway"); }
+// Mengambil variabel dari dashboard Railway
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
-$dbparts = parse_url($url);
-$conn = mysqli_connect($dbparts['host'], $dbparts['user'], $dbparts['pass'], ltrim($dbparts['path'], '/'), $dbparts['port']);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
-if (!$conn) { die("Koneksi gagal: " . mysqli_connect_error()); }
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
 ?>
