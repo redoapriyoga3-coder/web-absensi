065c0a393e0c356c8281b40f42df7792278d62c3
redoapriyoga3-coder <redoapriyoga3@gmail.com>
2026-07-10 12:48:16 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,17 +1,12 @@
 <?php
 session_start();
-
-// Data login sederhana (nanti kita ganti dengan database)
-$username_benar = "guru";
-$password_benar = "12345";
-
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if ($username == $username_benar && $password == $password_benar) {
+if ($username == "guru" && $password == "12345") {
     $_SESSION['login'] = true;
     header("Location: dashboard.php");
 } else {
-    echo "Login gagal! <a href='index.php'>Kembali ke Login</a>";
+    echo "Login Gagal! <a href='index.php'>Kembali</a>";
 }
 ?>
