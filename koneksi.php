a2121e489c826f22b188c8f5441a5938962fe213
redoapriyoga3-coder <redoapriyoga3@gmail.com>
2026-07-10 11:54:20 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+// Mengambil data dari Railway Environment Variables
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
+
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
+
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
