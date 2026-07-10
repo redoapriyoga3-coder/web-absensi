4aecca1612794f6a93eeb87749a7dd3b6ff2cde3
redoapriyoga3-coder <redoapriyoga3@gmail.com>
2026-07-10 11:51:52 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,11 @@
 <?php
-// login.php
 session_start();
-
-// Simulasi login sederhana (nanti kita hubungkan ke database sungguhan)
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 if ($username == "guru" && $password == "12345") {
     $_SESSION['login'] = true;
-    echo "Login Berhasil! Selamat datang, Guru.";
+    header("Location: dashboard.php"); // Otomatis pindah ke dashboard
 } else {
     echo "Username atau Password salah! <a href='index.php'>Kembali</a>";
 }
